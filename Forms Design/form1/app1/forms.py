5f10aa25d8ab754afaffd11e8fedde53abf7199e
alamin <alaminbhuyan321@gmail.com>
2021-12-07 09:43:36 +0600

    work on formsDesign>form1

--- Forms Design/form1/app1/forms.py
+++ Forms Design/form1/app1/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from django.core import validators
+import re
+import string
 
 
 
 # Custom form validators
 # comment: for UserName
+def checkPunctuation(value):
+    if re.search(pattern='[!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]', string=value):
+        raise forms.ValidationError(message="UserName can't contain any Punctuation")
+
 def checkSpace(value):
     if ' ' in value:
         raise forms.ValidationError(message="UserName can't contain any space")
@@ -17,30 +23,30 @@ def checkOnlyDigits_fn(value):
     if value.isdigit():
         raise forms.ValidationError(message="FirstName can't contain Only digits")
 
-# def checkOnlyDigits_fn2(value):
-#     if value.isalnum():
-#         raise forms.ValidationError(message="FirstName can't contain any digits")
+def checkAnyDigits_fn2(value):
+    if re.search(pattern="\d", string=value):
+        raise forms.ValidationError(message="FirstName can't contain any digits")
 
 def checkOnlyDigits_ln(value):
     if value.isdigit():
         raise forms.ValidationError(message="LastName can't contain Only digits")
 
-# def checkOnlyDigits_ln2(value):
-#     if value.isalnum():
-#         raise forms.ValidationError(message="LastName can't contain any digits")
+def checkAnyDigits_ln2(value):
+    if re.search(pattern="\d", string=value):
+        raise forms.ValidationError(message="LastName can't contain any digits")
 
 class UserForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder' : 'UserName'}), error_messages={'required':'Enter Your UserName'}, validators=[checkSpace, checkOnlyDigits])
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder' : 'UserName'}), error_messages={'required':'Enter Your UserName'}, validators=[checkSpace, checkOnlyDigits,checkPunctuation])
 
-    first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder' : 'FirstName'}), error_messages={'required':'Enter Your First Name'}, validators=[checkOnlyDigits_fn])
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder' : 'FirstName'}), error_messages={'required':'Enter Your First Name'}, validators=[checkOnlyDigits_fn,checkAnyDigits_fn2])
 
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder' : 'LastName'}), error_messages={'required':'Enter Your Last Name'}, validators=[checkOnlyDigits_ln])
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder' : 'LastName'}), error_messages={'required':'Enter Your Last Name'}, validators=[checkOnlyDigits_ln,checkAnyDigits_ln2])
 
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder' : 'Email'}), error_messages={'required':'Enter Your Email'})
 
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}), error_messages={'required':'Enter Your Password'})
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}), error_messages={'required':'Enter Your Password'}, validators=[validators.MinLengthValidator(5), validators.MaxLengthValidator(30)])
 
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Confirm Password'}), label="Confirm Password:", error_messages={'required':'Enter Your Confirm Password'})
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Confirm Password'}), label="Confirm Password:", error_messages={'required':'Enter Your Confirm Password'}, validators=[validators.MinLengthValidator(5), validators.MaxLengthValidator(30)])
 
     # comment: apply some restriction my own will
     # def clean_password(self):
